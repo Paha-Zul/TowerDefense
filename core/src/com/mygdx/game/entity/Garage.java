7d5866df2ffbd4ec7559aecc68966d014fc18e67
Pahasz <Bbenton91@gmail.com>
2015-02-03 06:02:19 -0500

    Extended more objects.

--- core/src/com/mygdx/game/entity/Garage.java
+++ core/src/com/mygdx/game/entity/Garage.java
@@ -1,7 +1,12 @@
 package com.mygdx.game.entity;
 
+import com.badlogic.gdx.math.Vector2;
+
 /**
  * Created by Paha on 2/3/2015.
  */
-public class Garage {
+public class Garage extends Entity{
+    public Garage(Vector2 position, float rotation) {
+        super(position, rotation);
+    }
 }
--- core/src/com/mygdx/game/entity/Mine.java
+++ core/src/com/mygdx/game/entity/Mine.java
@@ -1,7 +1,12 @@
 package com.mygdx.game.entity;
 
+import com.badlogic.gdx.math.Vector2;
+
 /**
  * Created by Paha on 2/3/2015.
  */
-public class Mine {
+public class Mine extends Entity{
+    public Mine(Vector2 position, float rotation) {
+        super(position, rotation);
+    }
 }
